6f6267debe6637b88d58ded74b2be5de8554c9f5
Ilya Vatsevich <leone999@mail.ru>
2023-01-10 19:02:13 +0300

    add default

---
+++ src/main/java/com/example/user_ingredient_service/UserIngredientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_ingredient_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserIngredientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserIngredientServiceApplication.class, args);
+    }
+
+}
